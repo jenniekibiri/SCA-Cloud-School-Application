d2b133a544932cf064250437c5bc6d7580813234
jenniekibiri <josphatje@gmail.com>
2022-04-01 13:55:07 +0300

    change display message

--- docker/src/App.js
+++ docker/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <p>Welcome to SCA Cloud School Application</p>
+     <p>Welcome to SCA Cloud School Application , this is my first assessment</p>
     </div>
   );
 }
